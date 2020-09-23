6ab562a6aeb2ca1b3e912d0da1a10d859e6a9018
Samuel2156 <71663060+Samuel2156@users.noreply.github.com>
2020-09-23 09:53:04 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,60 @@
 input.onGesture(Gesture.Shake, function () {
     roll = randint(1, 6)
     basic.showNumber(roll)
+    if (roll == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . # .
+            . # . . .
+            . . . . .
+            `)
+    }
+    if (roll == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (roll == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (roll == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (roll == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (roll == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
 })
 let roll = 0
 basic.showString("Shake")
