0876a7a9c4057f5370a8cb94b896a5ebff8082dc
Trevor Shaw <shawt@genlrn.com>
2020-09-23 11:35:28 -0400

    Teacher feedback - you can start your game.

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 input.onGesture(Gesture.Shake, function () {
     roll = randint(1, 6)
+    // This is cool. So you will show the Arabic numeral before displaying the dots. Was this on purpose?
     basic.showNumber(roll)
+    // Not a bad approach, but it is generally better to do something like this as a single if/then/else block and to handle each condition in order. It works fine this way because you are evaluating whether a value EQUALS something. It would be harder to do it this way if you were checking something like LESS THAN or GREATER THAN.
     if (roll == 3) {
         basic.showLeds(`
             . . . . .
